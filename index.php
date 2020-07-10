06d0334d6f670073d1ed2c732cf2f0a5f9eec68c
koudai <61653253+koudai0619@users.noreply.github.com>
2020-07-11 03:27:15 +0900

    ひととりは見た

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 
 <head>
     <meta charset="UTF-8">
@@ -11,25 +11,31 @@
     <?php
     try {
         $db = new PDO('mysql:dbname=mydb;host=127.0.0.1;charset=utf8', 'root', '');
+
+        $db->exec('INSERT INTO sample SET name="' . $_POST['memo'] . '"');
     } catch (PDOException $e) {
         echo 'DB接続エラー: ' .  $e->getMessage();
     }
+
+    // $db->exec('INSERT INTO  sample SET name="はなお"'); //データの追加
+
+    // $records = $db->query('SELECT * FROM sample'); //query構文はsqlによって得られたデータを戻り値として取得する
+    // while ($record = $records->fetch()) { //fetchは一行ずつ取り出していきデータがなくなったらfalseとなる
+    //     print($record['name'] . "\n");
+    // }
+
+
+
+
+    /*
+PDO:PHP Data Object　データベースを扱うためのオブジェクト
+PDO('データベースの名前;ローカルホストのIPアドレス;文字コード','ユーザー名','パスワード');
+データベースを扱うときは例外処理で扱うのが基本
+
+*/
 
 
     ?>
 </body>
 
 </html>
-
-/*
-
-PDO:PHP Data Object　データベースを扱うためのオブジェクト
-PDO('データベースの名前;ローカルホストのIPアドレス;文字コード','ユーザー名','パスワード');
-データベースを扱うときは例外処理で扱うのが基本
-
-
-
-
-
-
-*/
---
+++ twitter/templates/join/index.php
@@ -0,0 +1,72 @@
+<?php
+if (!empty($_POST))
+	if ($_POST['name'] === '') {
+		$error['name'] = 'blank';
+	}
+if ($_POST['email'] === '') {
+	$error['email'] = 'blank';
+}
+if (strlen($_POST['password']) < 4) {
+	$error['password'] = 'length';
+}
+if ($_POST['password']) {
+	$error['password'] = 'blank';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>会員登録</title>
+
+	<link rel="stylesheet" href="../style.css" />
+</head>
+
+<body>
+	<div id="wrap">
+		<div id="head">
+			<h1>会員登録</h1>
+		</div>
+
+		<div id="content">
+			<p>次のフォームに必要事項をご記入ください。</p>
+			<form action="" method="post" enctype="multipart/form-data">
+				<dl>
+					<dt>ニックネーム<span class="required">必須</span></dt>
+					<dd>
+						<input type="text" name="name" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['name'], ENT_QUOTES)); ?>" />
+						<?php
+						if ($error['name'] === 'blank') : ?>
+							<p class="error">*ニックネームを入力してください</p>
+						<?php endif; ?>
+					</dd>
+					<dt>メールアドレス<span class="required">必須</span></dt>
+					<dd>
+						<input type="text" name="email" size="35" maxlength="255" value="" />
+						<?php
+						if ($error['email'] === 'blank') : ?>
+							<p class="error">*メールアドレスを入力してください</p>
+						<?php endif; ?>
+					<dt>パスワード<span class="required">必須</span></dt>
+					<dd>
+						<input type="password" name="password" size="10" maxlength="20" value="" />
+						<?php
+						if ($error['password'] === 'blank') : ?>
+							<p class="error">*パスワードを入力してください</p>
+						<?php endif; ?>
+					</dd>
+					<dt>写真など</dt>
+					<dd>
+						<input type="file" name="image" size="35" value="test" />
+					</dd>
+				</dl>
+				<div><input type="submit" value="入力内容を確認する" /></div>
+			</form>
+		</div>
+</body>
+
+</html>
